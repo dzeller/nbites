dac268ba2b5b28fa7e16f3ba210fa1e0c84129c0
Daniel <dzeller@bowdoin.edu>
2015-07-01 23:08:15 -0400

    compiling man with native compiler. Next challenge is to link

--- src/share/cmake/atom.cmake
+++ src/share/cmake/atom.cmake
@@ -19,7 +19,7 @@ set( CMAKE_SYSTEM_NAME      Linux )
 set( CMAKE_SYSTEM_VERSION   1     )
 set( CMAKE_SYSTEM_PROCESSOR atom  )
 
-include("${TOOLCHAIN_DIR}/toolchain-atom.cmake")
+#include("${TOOLCHAIN_DIR}/toolchain-atom.cmake")
 
 set( CMAKE_CXX_FLAGS
   "${CMAKE_CXX_FLAGS} -march=core2 -mtune=generic -mssse3 -mfpmath=sse -fomit-frame-pointer -pipe")
