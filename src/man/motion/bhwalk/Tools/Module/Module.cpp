387f62f3c3b0a99fc788396158b6e1e8bce772e4
Daniel <dzeller@bowdoin.edu>
2015-07-07 14:13:56 -0400

    fixes segfault related to loading parameters

--- src/man/motion/bhwalk/Tools/Module/Module.cpp
+++ src/man/motion/bhwalk/Tools/Module/Module.cpp
@@ -15,16 +15,17 @@ void loadModuleParameters(Streamable& parameters, const char* moduleName, const 
   std::string name;
   if(!fileName)
   {
-    name = ModuleBase::config_path;
-    name += moduleName;
+    name = moduleName;
     name[0] = (char) tolower(name[0]);
     if(name.size() > 1 && isupper(name[1]))
       for(int i = 1; i + 1 < (int) name.size() && isupper(name[i + 1]); ++i)
         name[i] = (char) tolower(name[i]);
     name += ".cfg";
+    name = ModuleBase::config_path + name;
   }
-  else
+  else {
     name = fileName;
+  }
   InMapFile stream(name);
   ASSERT(stream.exists());
   stream >> parameters;
